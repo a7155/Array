2bebacc1013779940b23f54bdc4991897a8a7ef0
a7155 <114379365+a7155@users.noreply.github.com>
2023-03-17 11:41:53 +0700

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        // membuat array buah-buahan
+        String[] barang = new String[5];
+
+        // membuat scanner
+        Scanner scan = new Scanner(System.in);
+
+        // mengisi data ke array
+        for( int i = 1; i < barang.length; i++ ){
+            System.out.print("Kata ke-" + i + ": ");
+            barang[i] = scan.nextLine();
+        }
+
+        System.out.println("---------------------------");
+
+        // menampilkan semua isi array
+        for( String x : barang ){
+            System.out.println(x);
+        }
+
+    }
+}
--- Array2.java
+++ Array2.java
@@ -1,27 +1,38 @@
-
 import java.util.Scanner;
+public class Array2
+{
+   public static void main (String args[])
+   {
+    Scanner input=new Scanner(System.in);
 
-public class Array2 {
-    public static void main(String[] args) {
+    System.out.print("Masukan Jumlah Mahasiswa : ");
+    int n=input.nextInt();
 
-        // membuat array buah-buahan
-        String[] barang = new String[5];
+    String nama[]=new String[n];
+    String status[]=new String[n];
+    int nilai[]=new int[n];
 
-        // membuat scanner
-        Scanner scan = new Scanner(System.in);
+    for (int i=0;i<n;i++){
+         System.out.println("Mahasiswa Ke : "+(i+1));
+        System.out.print("Nama : ");
+        nama[i]=input.next();
 
-        // mengisi data ke array
-        for( int i = 1; i < barang.length; i++ ){
-            System.out.print("Kata ke-" + i + ": ");
-            barang[i] = scan.nextLine();
-        }
+        System.out.print("Nilai : ");
+        nilai[i]=input.nextInt();
 
-        System.out.println("---------------------------");
-
-        // menampilkan semua isi array
-        for( String x : barang ){
-            System.out.println(x);
-        }
-
+        if (nilai[i]<=50) {
+                status[i]="Tidak Lulus";
+            } else {
+                status[i]="Lulus";
+            }
     }
-}
+    System.out.println("DAFTAR NILAI MAHASISWA");
+    System.out.println("======================");
+    System.out.println("No   Nama   Nilai   Status  ");
+    
+    for (int i=0; i<n;i++) {
+        System.out.println((i+1)+"    "+nama[i]+"     "+nilai[i]+"       "+status[i]);
+    }
+    
+    }
+ }
